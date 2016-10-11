48f000030581c9081e04dbd022579ccbfe077ca5
AppGeek <sed.mohsen.moosavi@gmail.com>
2016-10-11 15:42:19 +0330

    اتصال بین پپ آپ و بکگراند / ایجاد ظاهر پپ آپگ

--- bg.js
+++ bg.js
@@ -1,4 +1,3 @@
-
 /*
 chrome.webRequest.onCompleted.addListener(function(details) {
     console.log(details);
@@ -6,50 +5,55 @@ chrome.webRequest.onCompleted.addListener(function(details) {
     urls: ["<all_urls>"]
 });
 */
-var ports = {};
+var ports = {},
+    popupPort;
 
 function isFunction(obj) {
   return !!(obj && obj.constructor && obj.call && obj.apply);
 };
 
 function guidGenerator() {
-    var S4 = function() {
-       return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
-    };
-    return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+  var S4 = function () {
+    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+  };
+  return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
 }
 
 var PipelineService = {
-  pipelines :{},
-  Create:function(tabId,onend){
-    var port = ports[tabId]; 
-    this.pipelines[tabId] = new Pipeline(port,onend);
+  pipelines: {},
+  Create: function (tabId, onend) {
+    var port = ports[tabId];
+    this.pipelines[tabId] = new Pipeline(port, onend);
     return this.pipelines[tabId];
   },
-  Callback:function(tabId,callbackId,response){
-    this.pipelines[tabId].Invoke(callbackId,response);
+  Callback: function (tabId, callbackId, response) {
+    this.pipelines[tabId].Invoke(callbackId, response);
   }
 }
 
-var Pipeline = function(port,onend){
+var Pipeline = function (port, onend) {
   this.fns = new Array();
   this.index = 0;
   this.port = port;
-  this.onend = onend || function(){};
+  this.onend = onend || function () {};
 }
 
-Pipeline.prototype.Register = function(action,fn){
-  this.fns.push({id:guidGenerator(),action:action,callback:fn});
+Pipeline.prototype.Register = function (action, fn) {
+  this.fns.push({
+    id: guidGenerator(),
+    action: action,
+    callback: fn
+  });
   return this;
 };
 
-Pipeline.prototype.Start = function(){
+Pipeline.prototype.Start = function () {
   this.index = -1;
   return this.Next();
 }
-Pipeline.prototype.Invoke = function(callbackId,response){
-  for(var i in this.fns){
-    if(this.fns[i].id==callbackId){
+Pipeline.prototype.Invoke = function (callbackId, response) {
+  for (var i in this.fns) {
+    if (this.fns[i].id == callbackId) {
       this.fns[i].callback(response);
       break;
     }
@@ -57,17 +61,20 @@ Pipeline.prototype.Invoke = function(callbackId,response){
   this.Next();
 }
 
-Pipeline.prototype.Next = function(){
+Pipeline.prototype.Next = function () {
   this.index++;
-  if( this.index <this.fns.length){
-    this.port.postMessage({action: this.fns[this.index].action,callbackId:this.fns[this.index].id});
-  }else{
+  if (this.index < this.fns.length) {
+    this.port.postMessage({
+      action: this.fns[this.index].action,
+      callbackId: this.fns[this.index].id
+    });
+  } else {
     this.onend();
   }
   return this;
 }
 
-function updateFollowers(nodes){
+function updateFollowers(nodes) {
   var transaction = db.transaction(["followers"], "readwrite");
   /*
   chrome.storage.local.remove('followers');
@@ -76,97 +83,124 @@ function updateFollowers(nodes){
                               console.log('Settings saved');
                             });
                             */
-  transaction.oncomplete = function(event) {
+  transaction.oncomplete = function (event) {
     alert("All done!");
   };
 
-  transaction.onerror = function(event) {
+  transaction.onerror = function (event) {
     // Don't forget to handle errors!
   };
 
   var objectStore = transaction.objectStore("followers");
   for (var i in nodes) {
     var request = objectStore.add(nodes[i]);
-    request.onsuccess = function(event) {
+    request.onsuccess = function (event) {
 
     };
   }
 }
 
 var instaext = {
-    Pipeline:{},
-    Update: function(sendResponse){;
-       
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            //GotoHomePage > onEnd GetFollowersCount > onEnd GetFollowingsCount ...
+  Pipeline: {},
+  Update: function (sendResponse) {;
 
-            var pipeline = PipelineService.Create(tabs[0].id,sendResponse)
+    chrome.tabs.query({
+      active: true,
+      currentWindow: true
+    }, function (tabs) {
+      //GotoHomePage > onEnd GetFollowersCount > onEnd GetFollowingsCount ...
 
-                          // .Register("GotoHomePage",function(response) {
-                          //   console.log(response);
-                          // })
-                var nodes = []
-                var recursiveGetFollowers = function(response){
-                            //console.log("Open followers>"+response.Query);
-                            var query = JSON.parse(JSON.parse(response.Query).responseText);
-                            nodes = nodes.concat(query.followed_by.nodes);
-                            if(query.status=="ok"&&query.followed_by.page_info.has_next_page){
-                              pipeline.Register("GetNextFollowers",recursiveGetFollowers);
-                            }else{
-                              updateFollowers(nodes);
-                            }
-                          };
+      var pipeline = PipelineService.Create(tabs[0].id, sendResponse)
 
-                pipeline.Register("GetFollowersCount",function(response) {
-                            console.log("Followers Count:" +  response.Result);
-                          })
-                          .Register("GetFollowingsCount",function(response) {
-                            console.log("Followings Count:" +  response.Result);
-                          })
-                          .Register("OpenFollowersDialog",recursiveGetFollowers)
-                          .Start();
-        });
+      // .Register("GotoHomePage",function(response) {
+      //   console.log(response);
+      // })
+      var nodes = []
+      var recursiveGetFollowers = function (response) {
+        //console.log("Open followers>"+response.Query);
+        var query = JSON.parse(JSON.parse(response.Query).responseText);
+        nodes = nodes.concat(query.followed_by.nodes);
+        if (query.status == "ok" && query.followed_by.page_info.has_next_page) {
+          pipeline.Register("GetNextFollowers", recursiveGetFollowers);
+        } else {
+          updateFollowers(nodes);
+        }
+      };
 
-        
-    }
+      pipeline.Register("GetFollowersCount", function (response) {
+          console.log("Followers Count:" + response.Result);
+        })
+        .Register("GetFollowingsCount", function (response) {
+          console.log("Followings Count:" + response.Result);
+        })
+        .Register("OpenFollowersDialog", recursiveGetFollowers)
+        .Start();
+    });
+
+
+  },
+  ActivationStatus: function(port,msg){
+    port.postMessage({
+      action:"Callback.ActivationStatus",
+      result:true
+    });
+  },
+  LoginStatus: function(port,msg){
+    port.postMessage({
+      action:"Callback.LoginStatus",
+      result:true
+    });
+  }
 };
 
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    switch (request.action){
+  function (request, sender, sendResponse) {
+    switch (request.action) {
       case "Update":
         instaext.Update(sendResponse);
         break;
     }
+  });
+
+chrome.runtime.onConnect.addListener(function (port) {
+  if (port.name == "popup") {
+    popupPort = port;
+    port.onMessage.addListener(function(msg){
+        instaext[msg.action].apply(instaext,[port,msg]);
+    });
+  } else {
+    ports[port.sender.tab.id] = port;
+    chrome.pageAction.show(port.sender.tab.id);
+    port.onMessage.addListener(function (msg) {
+      if (msg.action != undefined && msg.action != null) {
+        if (msg.action.indexOf("callback.") == 0) {
+          PipelineService.Callback(port.sender.tab.id, msg.action.split('.')[1], msg.response);
+        } else {
+
+        }
+      }
+    });
+  }
 });
 
-chrome.runtime.onConnect.addListener(function(port) {
-  ports[port.sender.tab.id] = port;
-  chrome.pageAction.show(port.sender.tab.id);
-  port.onMessage.addListener(function(msg) {
-    if(msg.action!=undefined&&msg.action!=null){
-      if(msg.action.indexOf("callback.")==0){
-        PipelineService.Callback(port.sender.tab.id,msg.action.split('.')[1],msg.response);
-      }else{
-        
-      }
-    }
-  });
-});
 var request = indexedDB.open('instaext', 2);
 var db;
-request.onerror = function(event) {
+request.onerror = function (event) {
   alert("Why didn't you allow my web app to use IndexedDB?!");
 };
-request.onsuccess = function(event) {
+request.onsuccess = function (event) {
   db = event.target.result;
   console.log('db succeeded');
 };
-request.onupgradeneeded = function(event) { 
+request.onupgradeneeded = function (event) {
   db = event.target.result;
 
   // Create an objectStore for this database
-  var objectStore = db.createObjectStore("followers", { keyPath: "id" });
-  objectStore.createIndex("username", "username", { unique: true });
+  var objectStore = db.createObjectStore("followers", {
+    keyPath: "id"
+  });
+  objectStore.createIndex("username", "username", {
+    unique: true
+  });
 
 };
--- popup.js
+++ popup.js
@@ -1,17 +1,83 @@
-document.getElementById("btn").onclick = function(e){
-  hello();
-}
-
 function hello() {
   console.log("hello");
   chrome.runtime.sendMessage({
       action: "Update"
     },
-    function(response) {
+    function (response) {
       document.getElementById("div").textContent = response.msg;
     });
 }
-var i =0;
-window.onload = function(){
+var i = 0;
+window.onload = function () {
   document.getElementById("div").textContent = i++;
+  document.getElementById("btn").onclick = function (e) {
+    hello();
+  }
+
 }
+
+function showSection(id) {
+  $('section.active').removeClass('active');
+  $('section' + id).addClass('active');
+}
+
+var port = chrome.extension.connect({
+  name: "popup"
+});
+
+var instaext = {
+  ActivationStatus: function (msg) {
+    if (msg.result) {
+      port.postMessage({
+        action: "LoginStatus"
+      });
+    } else {
+      showSection('#activation');
+    }
+  },
+  LoginStatus: function (msg) {
+    if (msg.result) {
+      showSection('#main');
+    } else {
+      showSection('#login');
+    }
+  }
+
+};
+
+port.onMessage.addListener(function (msg) {
+  if (msg.action != undefined && msg.action != null) {
+    if (msg.action.indexOf("Callback.") == 0) {
+      instaext[msg.action.split('.')[1]].apply(instaext, [msg]);
+    } else {
+
+    }
+  }
+});
+
+//initialize
+Zepto(function ($) {
+
+  $('a.button-return').on('click', function (e) {
+    e.preventDefault();
+    showSection('#main');
+  });
+
+  $("section#main a").on('click', function (e) {
+    e.preventDefault();
+    var $this = $(this);
+    showSection($this.prop('target'));
+  });
+
+  $('#follow-pattern').on('change', function (e) {
+    var $this = $(this);
+    $('#follow-pattern-manual').toggle($this.val() == 'manual');
+    $('#follow-pattern-auto').toggle($this.val() == 'auto');
+  });
+
+  //get activation status
+  port.postMessage({
+    action: "ActivationStatus"
+  });
+  //get login status
+})
